85f9e0397ee087db55468175382a3963d29623fc
yuuki <>
2024-07-30 13:36:17 +0000

    サーバーが停止しても自動で起動する

---
+++ running-forever/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+services=( taiko-web litey )
+
+for service in ${services[@]}; do
+  (
+    cd "../docker/$service"
+    tmux kill-session -t "$service"
+    tmux new-session -s "$service" -d
+    tmux send-keys -t "$service" "while true; do docker compose down; docker compose up --abort-on-container-exit; done" C-m
+  )
+done
